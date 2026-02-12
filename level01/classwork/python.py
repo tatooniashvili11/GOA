63dcc38eee2765fa1ea975b1643bcc1dca946c18
tatooniashvili11 <stufystufy993@gmail.com>
2026-02-12 22:36:18 +0400

    first commit

---
+++ level01/classwork/python.py
@@ -0,0 +1,6 @@
+name = "tato"
+lastname = 'oniashvili'
+age = 13
+print(name)
+print(lastname)
+print(age) 
---
+++ level02/classwork/main.py
@@ -0,0 +1,27 @@
+#1-? 
+#task 2
+age = 2
+number = -4 
+
+
+#task 3
+sum = 30 + 30
+subtract = 20 - 14
+multiplied = 20*3
+divided = 12/6
+
+
+#task 4
+age = 13
+age = age + 35
+
+age = age
+age = 13 - 20
+
+age - 13
+age = age + 35
+age = age - 20
+
+age = age * 20
+age = age / 5
+
